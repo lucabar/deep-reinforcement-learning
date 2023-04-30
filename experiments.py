b664da9f2d4c8535ec2abcc470f2d623b2268393
Chris Tsirogiannis <christsirogiannis@gmail.com>
2023-04-30 11:40:53 +0200

    adds argv for executing experiments of part 2

--- experiments.py
+++ experiments.py
@@ -21,75 +21,115 @@ eta = 0.0005
 seed = 13
 obs_type = 'pixel'
 
+
 # good parameter run -> plot
-boot = ["MC","MC","n_step","n_step"]
+boot = ["MC", "MC", "n_step", "n_step"]
 baseline = [False, True, False, True]
-i = int(args[0])  # we could also write a very short shell file that only takes an integer as CLI
-# python experiments.py 1
-# python experiments.py 2
-#to run the experiments separately run "exp.sh"
+# we could also write a very short shell file that only takes an integer as CLI
+section = int(args[0])
+i = int(args[1])
 
-# or just change it back to the way it was
-# for i in [0,1,2,3]:
+'''
+python experiments.py 1
+python experiments.py 2
+to run the experiments separately run "exp.sh"
 
-for j in range(2):
-    stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
-    print(f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
+or just change it back to the way it was
+for i in [0,1,2,3]:
+'''
+if section == 0:
+    for j in range(1):
+        stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
+        print(
+            f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
 
-    rewards = reinforce(n_episodes, learning_rate, rows, columns, obs_type,
-                        max_misses, max_steps, seed, n_step, speed, boot[i], 
-                        P_weights, V_weights, minibatch, eta, stamp, baseline[i])
-
-    with open("data/documentation.txt", 'a') as f:
-            f.write(
-                f'\n\n {stamp}, Exp{i},{j} ... params: {reinforce.params}, Avg reward: {np.mean(rewards)} \n')
-            
-### here PART 1 finishes
-
-### PART 2 begins
-
-boot = "n_step"
-baseline = True
-# Experiment 1 - Size Variation
-# square 7x7, square 14x14, rectangle 7x14, rectangle 14x7
-list_of_rows_columns = [(7, 7), (14, 14), (7, 14), (14, 7)]
-
-
-for j in range(5):
-    stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
-    print(f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
-    for rows, columns in list_of_rows_columns:
-        rows = list_of_rows_columns[i][0]
-        columns = list_of_rows_columns[i][1]
         rewards = reinforce(n_episodes, learning_rate, rows, columns, obs_type,
-                            max_misses, max_steps, seed, n_step, speed, boot[i], 
+                            max_misses, max_steps, seed, n_step, speed, boot[i],
                             P_weights, V_weights, minibatch, eta, stamp, baseline[i])
 
-    with open("data/documentation.txt", 'a') as f:
+        with open("data/documentation.txt", 'a') as f:
             f.write(
                 f'\n\n {stamp}, Exp{i},{j} ... params: {reinforce.params}, Avg reward: {np.mean(rewards)} \n')
 
 
 
-# Experiment 2 - Speed Variation
-# velocity 0.5, 1.0, 1.5, 2.0
-speeds = [0.5, 1.0, 1.5, 2.0]
-for j in range(5):
-    stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
-    print(f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
-    for speed in speeds:
-        rewards = reinforce(n_episodes, learning_rate, rows, columns, obs_type,
-                            max_misses, max_steps, seed, n_step, speed, boot[i], 
-                            P_weights, V_weights, minibatch, eta, stamp, baseline[i])
+# PART 2
 
-    with open("data/documentation.txt", 'a') as f:
-            f.write(
-                f'\n\n {stamp}, Exp{i},{j} ... params: {reinforce.params}, Avg reward: {np.mean(rewards)} \n')
+if section == 1:
 
-# Experiment 3 - Observation Type
-obs_types = ['vector', 'pixel']
+    boot = "n_step"
+    baseline = True
 
-#Experiment 4 - Environment - Speed variation
-rows = 7
-columns = 14
-speeds = [0.5, 1.0, 2.0]
+    # Experiment 0 - Size Variation
+    # square 7x7, square 14x14, rectangle 7x14, rectangle 14x7
+    if i == 0:
+        list_of_rows_columns = [(7, 7), (14, 14), (7, 14), (14, 7)]
+
+        for rows, columns in list_of_rows_columns:
+            for j in range(5):
+                stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
+                print(
+                    f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
+                rewards = reinforce(n_episodes, learning_rate, rows, columns, obs_type,
+                                    max_misses, max_steps, seed, n_step, speed, boot,
+                                    P_weights, V_weights, minibatch, eta, stamp, baseline)
+
+                with open("data/documentation.txt", 'a') as f:
+                    f.write(
+                        f'\n\n {stamp}, Exp{i},{j},{rows}x{columns} ... params: {reinforce.params}, Avg reward: {np.mean(rewards)} \n')
+
+
+    # Experiment 1 - Speed Variation
+    # velocity 0.5, 1.0, 1.5, 2.0
+
+    if i == 1:
+        speeds = [0.5, 1.0, 1.5, 2.0]
+        for speed in speeds:
+            for j in range(5):
+                stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
+                print(
+                    f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
+                rewards = reinforce(n_episodes, learning_rate, rows, columns, obs_type,
+                                    max_misses, max_steps, seed, n_step, speed, boot,
+                                    P_weights, V_weights, minibatch, eta, stamp, baseline)
+
+                with open("data/documentation.txt", 'a') as f:
+                    f.write(
+                        f'\n\n {stamp}, Exp{i},{j},{speed} ... params: {reinforce.params}, Avg reward: {np.mean(rewards)} \n')
+
+    # Experiment 2 - Observation Type
+
+    if i == 2:
+        obs_types = ['vector', 'pixel']
+        for obs_type in obs_types:
+            for j in range(5):
+                stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
+                print(
+                    f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
+                rewards = reinforce(n_episodes, learning_rate, rows, columns, obs_type,
+                                    max_misses, max_steps, seed, n_step, speed, boot,
+                                    P_weights, V_weights, minibatch, eta, stamp, baseline)
+
+                with open("data/documentation.txt", 'a') as f:
+                    f.write(
+                        f'\n\n {stamp}, Exp{i},{j} ... params: {reinforce.params}, Avg reward: {np.mean(rewards)} \n')
+
+    # Experiment 3 - Environment - Speed variation
+
+    if i == 3:
+        rows = 7
+        columns = 14
+        speeds = [0.5, 1.0, 2.0]
+
+        for speed in speeds:
+            for j in range(5):
+                stamp = time.strftime("%d_%H%M%S", time.gmtime(time.time()))
+                print(
+                    f"\n\n === Running Experiment No.{i}, Rep.{j} === \n Stamp: {stamp} \n\n")
+                rewards = reinforce(n_episodes, learning_rate, rows, columns, obs_type,
+                                    max_misses, max_steps, seed, n_step, speed, boot,
+                                    P_weights, V_weights, minibatch, eta, stamp, baseline)
+
+                with open("data/documentation.txt", 'a') as f:
+                    f.write(
+                        f'\n\n {stamp}, Exp{i},{j} ... params: {reinforce.params}, Avg reward: {np.mean(rewards)} \n')
