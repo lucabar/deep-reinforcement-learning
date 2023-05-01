8f8478f939db5a9a68b8bd00143ad0cbcb6f42e7
lucabar <lucabarbera@posteo.de>
2023-05-01 13:04:48 +0200

    speeds done

--- fullplot.py
+++ fullplot.py
@@ -4,6 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
+def load_and_mean(files: list[str]) -> list:
+    rewards = []
+    for plot in files:
+        rewards.append(np.load(f'data/rewards/r_{plot}.npy'))
+    return np.mean(rewards,axis=0)
+
 colors = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink','tab:grey','tab:olive','tab:cyan']
 
 
@@ -84,15 +90,18 @@ plt.show()
 
 
 ### PART 2
-default = ['30_202045','30_204133','30_210223','30_212339']
-rewards = []
-for plot in default:
-    rewards.append(np.load(f'data/rewards/r_{plot}.npy'))
-rewards = np.mean(rewards,axis=0)
-plt.plot(savgol_filter(rewards, 10,1), label = 'default')
+default = ['30_202045','30_204133','30_210223','30_212339','01_065527']
+speed_05 = ['30_113517','30_115449','30_121431','30_123431','30_140008']
+speed_15 = ['30_151006','30_151743','30_152617','30_153421','30_142345']
+speed_20 = ['30_154243','30_154719','30_155202','30_155709']
 
-plt.ylim(0,36)
-plt.title('Default settings, no training')
+plt.plot(savgol_filter(load_and_mean(default), 10,1), label = 'default')
+plt.plot(savgol_filter(load_and_mean(speed_05), 10,1), label = 'speed 0.5')
+plt.plot(savgol_filter(load_and_mean(speed_15), 10,1), label = 'speed 1.5')
+plt.plot(savgol_filter(load_and_mean(speed_20), 10,1), label = 'speed 2.0')
+
+plt.ylim(-10,36)
+plt.title('Default vs speed 0.5')
 plt.grid()
 plt.legend()
 plt.show()
