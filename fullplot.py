4975d250809dbcddd76f8cd1029160ada4b44826
lucabar <lucabarbera@posteo.de>
2023-05-05 14:28:14 +0200

    plots w confidence

--- fullplot.py
+++ fullplot.py
@@ -17,28 +17,24 @@ def load_list(files: list[str]) -> list:
         rewards.append(rew)
     return np.array(rewards)
 
-def plot_list_errors(list_strings: list[str], save_name: str = None):
+def plot_list(list_strings: list[str],label: str = None, color: str = None):
+    rewards = load_and_mean(list_strings)
+    plt.plot(savgol_filter(rewards,10,1),label=label, color=color)
+
+def plot_list_errors(list_strings: list[str], save_name: str = None,color:str='#BEBEBE'):
     reward = load_list(list_strings)
     # Calculate the mean and standard deviation of the data
     means = np.mean(reward, axis=0)
     stds = np.std(reward, axis=0, ddof=1)
     # Calculate the standard error of the mean
     sem = stds / np.sqrt(reward.shape[1])
-
+    sem = abs(sem)
     # Calculate the 95% confidence interval
     conf_int = 1.96 * sem
 
     # Plot the means with error bars representing the confidence interval
-    plt.errorbar(np.arange(reward.shape[1]), means, yerr=conf_int, fmt='o', capsize=1,color='tab:red')
-    plt.plot(np.arange(reward.shape[1]),np.mean(reward,axis=0))
-
-    plt.ylim(-10,36)
-    plt.title('Default vs speed 0.5')
-    plt.grid()
-    plt.legend()
-    if save_name:
-        plt.savefig(save_name)
-    plt.show()
+    # plt.errorbar(np.arange(reward.shape[1]), savgol_filter(means,10,1), yerr=conf_int, fmt='o', capsize=5,color='tab:red',markersize=0.2)
+    plt.fill_between(np.arange(means.shape[0]),savgol_filter(means,10,1)-conf_int,savgol_filter(means,10,1)+conf_int,alpha=0.3,color=color)
 
 colors = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown','tab:pink','tab:grey','tab:olive','tab:cyan']
 
@@ -46,10 +42,10 @@ colors = ['tab:blue','tab:orange','tab:green','tab:red','tab:purple','tab:brown'
 ### Part 1
 
 
-part1_full = ['04_183040','05_075841']
 part1_reinforce = ['04_215854']
-part1_bootstrap = ['05_034633','05_053914']
 part1_MCbaseline = ['04_225730','05_004942']
+part1_bootstrap = ['05_034633','05_053914']
+part1_full = ['04_183040','05_075841']
 
 hyperopt_big = ['21_205207','21_224331','21_205312','22_002411','22_015841','22_021350','22_042033','22_054051','22_063949']
 hyperopt_big += ['22_072616','22_083833','22_101711','22_094037','22_140345','22_163358','22_180822','22_194411','22_211909']
@@ -58,32 +54,20 @@ hyperopt_big += ['22_231129','23_004433']
 learning_plots = ['23_131422','23_151619','23_203642','23_225132']  # with 0.01 eta
 eta_plots = ['26_171012','26_151751','26_175224']  # with 0.001 learning '25_181333','25_201320','25_221342' more runs but bad
 
-rewards = []
-for plot in part1_reinforce:
-    rewards.append(np.load(f'data/rewards/r_{plot}.npy'))
-rewards = np.mean(rewards,axis=0)
-plt.plot(savgol_filter(rewards, 10,1), label='REINFORCE')
+plot_list(part1_reinforce,'REINFORCE',color='tab:blue')
+plot_list_errors(part1_reinforce,color='tab:blue')
 
 
-rewards = []
-for plot in part1_MCbaseline:
-    rewards.append(np.load(f'data/rewards/r_{plot}.npy'))
-rewards = np.mean(rewards,axis=0)
-plt.plot(savgol_filter(rewards, 10,1), label = 'MC baseline')
+plot_list(part1_MCbaseline,'MC baseline',color='tab:orange')
+plot_list_errors(part1_MCbaseline,color='tab:orange')
 
 
-rewards = []
-for plot in part1_bootstrap:
-    rewards.append(np.load(f'data/rewards/r_{plot}.npy'))
-rewards = np.mean(rewards,axis=0)
-plt.plot(savgol_filter(rewards, 10,1), label='bootstrap')
+plot_list(part1_bootstrap,'5-step bootstrap',color='tab:green')
+plot_list_errors(part1_bootstrap,color='tab:green')
 
 
-rewards = []
-for plot in part1_full:
-    rewards.append(np.load(f'data/rewards/r_{plot}.npy')[:250])
-rewards = np.mean(rewards,axis=0)
-plt.plot(savgol_filter(rewards, 10,1), label = 'full')
+plot_list(part1_full,'full','tab:red')
+plot_list_errors(part1_full,color='tab:red')
 
 plt.title('End of Part 1')
 plt.grid()
@@ -113,6 +97,24 @@ plt.show()
 
 
 ### PART 2
+nine_nine = ['04_210821','04_213900','04_221523','04_224709','04_232119']
+speed_05 = ['05_002934','05_012051','05_021216']
+
+plot_list(speed_05, 'speed = 0.5', color = 'tab:blue')
+plot_list(nine_nine,'shape = 9x9', color= 'tab:orange')
+plot_list_errors(speed_05, color = 'tab:blue')
+plot_list_errors(nine_nine, color= 'tab:orange')
+
+plt.title('some of part2')
+plt.grid()
+plt.xlabel('Episode')
+plt.ylabel('Reward')
+plt.legend(loc='best')  # fontsize='8'
+plt.savefig('plots/part2.pdf')
+plt.show()
+
+
+
 default = ['30_202045','30_204133','30_210223','30_212339','01_065527']
 speed_05 = ['30_113517','30_115449','30_121431','30_123431','30_140008']
 speed_15 = ['30_151006','30_151743','30_152617','30_153421','30_142345']
