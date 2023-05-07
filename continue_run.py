e3655bf06f5d14ba8eab5cddc090e50bc6bb1f21
lucabar <lucabarbera@posteo.de>
2023-05-07 11:51:17 +0200

    luca minor

--- continue_run.py
+++ continue_run.py
@@ -1,12 +1,10 @@
 from REINFORCE_semi import reinforce
 import time
 import numpy as np
-import sys
 
-args = sys.argv[1:]
 
 # game settings
-n_episodes = 50
+n_episodes = 150
 learning_rate = 0.01
 rows = 7
 columns = 7
@@ -18,7 +16,7 @@ minibatch = 4
 P_weights = None
 V_weights = None
 eta = 0.001
-obs_type = 'pixel'
+obs_type = 'vector'
 boot = 'n_step'
 baseline = True
 seed = np.random.randint(100)
@@ -26,8 +24,9 @@ training = True
 
 # give a list which weights you wanna run
 # weights = ['26_175224']  # eta runs. len: 128, 168, 165 
-weights = ['06_174531', '06_183121', '06_191736']  # full run runs (we want 400, current:350)
+# weights = ['06_174531', '06_183121', '06_191736']  # full run runs (we want 400, current:350)
 # weights = ['07_025544']  # naked run
+weights = ['06_103356','06_191148','06_202327']  # vectors
 
 for weight in weights:
     P_weights = f'data/weights/w_P_{weight}.h5'
